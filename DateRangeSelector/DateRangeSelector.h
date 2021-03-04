9f1da6400e65282bb5660b44c27889358e852e26
amirhoseinmrn <amirhoseinmrn1@gmail.com>
2021-03-04 15:24:52 +0330

    Initial project setup

---
+++ DateRangeSelector/DateRangeSelector.h
@@ -0,0 +1,18 @@
+//
+//  DateRangeSelector.h
+//  DateRangeSelector
+//
+//  Created by Amir on 3/4/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DateRangeSelector.
+FOUNDATION_EXPORT double DateRangeSelectorVersionNumber;
+
+//! Project version string for DateRangeSelector.
+FOUNDATION_EXPORT const unsigned char DateRangeSelectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DateRangeSelector/PublicHeader.h>
+
+
---
+++ DateRangeSelectorExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DateRangeSelectorExample
+//
+//  Created by Amir on 3/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
